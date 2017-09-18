a24cb1218d470fc7e14b24d20a9dd8a1d850af98
Haku Hal <hakuhal.snow@gmail.com>
2017-09-18 11:27:17 +0800

    finished the design first page function

--- js/objs.js
+++ js/objs.js
@@ -1,15 +1,151 @@
 /**
  * Created by hakuh on 2017/9/17.
  */
-var dataSet=[];
+var photoSet=[];
 obj1={
     title:"韩国，首尔",
     desc:"模拟博物馆，阿尔瓦罗·西扎作品。",
-    tag:["建筑"],
-    year:"2015",
+    tag:["2015","建筑"],
+    // year:"2015",
     catalog:"摄影",
     ava:'img/ava/architecture/_MG_9464.JPG',
-    work:''
+    work:'img/work/architecture/soul1-resize.JPG'
 };
 
-dataSet.push(obj1);
+photoSet.push(obj1);
+
+obj2={
+    title:"西班牙",
+    desc:"手机摄影。",
+    tag:["建筑"],
+    year:"2016",
+    catalog:"摄影",
+    ava:'img/ava/architecture/IMG_2443.JPG',
+    work:'img/work/architecture/spain1-resize.JPG'
+};
+
+photoSet.push(obj2);
+
+obj3={
+    title:"德国，柏林",
+    desc:"犹太人纪念碑。手机摄影。",
+    tag:["建筑"],
+    year:"2016",
+    catalog:"摄影",
+    ava:'img/ava/architecture/IMG_2750.JPG',
+    work:'img/work/architecture/berlin-resize.JPG'
+};
+
+photoSet.push(obj3);
+
+obj4={
+    title:"日本，高松",
+    desc:"高松筑港。手机摄影。",
+    tag:["建筑"],
+    year:"2016",
+    catalog:"摄影",
+    ava:'img/ava/architecture/IMG_4030.JPG',
+    work:'img/work/architecture/takamatsu-resize.JPG'
+};
+
+photoSet.push(obj4);
+
+obj5={
+    title:"日本，丰岛",
+    desc:"丰岛美术馆。手机摄影。",
+    tag:["建筑"],
+    year:"2016",
+    catalog:"摄影",
+    ava:'img/ava/architecture/IMG_8056.JPG',
+    work:'img/work/architecture/toyojima-resize.JPG'
+};
+
+photoSet.push(obj5);
+
+//卡片计划
+var designSet=[];
+objc1={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2017","卡片计划"],
+    // year:"2017",
+    catalog:"设计",
+    ava:'img/ava/card/0FE-resize.jpg',
+    work:'img/work/card/0FE-resize.jpg'
+};
+
+designSet.push(objc1);
+
+objc2={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2016","卡片计划"],
+    // year:"2016",
+    catalog:"设计",
+    ava:'img/ava/card/001-resize.jpg',
+    work:'img/work/card/001-resize.jpg'
+};
+
+designSet.push(objc2);
+
+objc3={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2015","卡片计划"],
+    // year:"2015",
+    catalog:"设计",
+    ava:'img/ava/card/004-resize.jpg',
+    work:'img/work/card/004-resize.jpg'
+};
+
+designSet.push(objc3);
+
+objc4={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2016","卡片计划"],
+    // year:"2016",
+    catalog:"设计",
+    ava:'img/ava/card/037-resize.jpg',
+    work:'img/work/card/037-resize.jpg'
+};
+
+designSet.push(objc4);
+
+objc5={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2013","卡片计划"],
+    // year:"2013",
+    catalog:"设计",
+    ava:'img/ava/card/039-resize.jpg',
+    work:'img/work/card/039-resize.jpg'
+};
+
+designSet.push(objc5);
+
+objc6={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2016","卡片计划"],
+    // year:"2016",
+    catalog:"设计",
+    ava:'img/ava/card/242-resize.jpg',
+    work:'img/work/card/242-resize.jpg'
+};
+
+designSet.push(objc6);
+
+
+
+objc7={
+    title:"卡片计划，设计",
+    desc:"卡片计划。",
+    tag:["2016","卡片计划"],
+    // year:"2016",
+    catalog:"设计",
+    ava:'img/ava/card/C21-resize.jpg',
+    work:'img/work/card/C21-resize.jpg'
+};
+
+designSet.push(objc7);
